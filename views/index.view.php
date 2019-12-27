8bdf34d79c12798cf35a72812fee9b7ef1819901
Andrea Morales Escobar <ame171337@gmail.com>
2019-12-26 20:39:25 -0600

    primeras configuraciones

--- views/index.view.php
+++ views/index.view.php
@@ -4,12 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>Villa Infantil MI</title>
 </head>
 <body>
-<div>
-    <img src="logo.svg" alt="Logo de la escuela">
-</div>  
+
+<nav>
+    <img src="img/logo.svg" alt="">
+    <h1>COLEGIO JESÚS ROMERO FLORES</h1>
+</nav>
+
+<!--Archivos javascript para bootstrap --> 
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
 </body>
 </html>

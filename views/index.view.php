fc7d399fc01dc8f757351dc02cba315fdc1ced7c
Andrea Morales Escobar <ame171337@gmail.com>
2019-12-29 00:51:40 -0600

    barra de navegacion para pc

--- views/index.view.php
+++ views/index.view.php
@@ -1,45 +1,224 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Villa Infantil MI</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Villa Infantil María Isabel</title>
+  <script defer src="js/all.js"></script>
 </head>
+
 <body>
-
-<nav>
-    <!-- <img src="img/logo.svg" alt=""> -->
-    <h1>COLEGIO JESÚS ROMERO FLORES</h1>
-</nav>
-<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-        <img src="img/img1.png" class="d-block w-100 img-slide" alt="...">
+  <header class="site-header">
+    <div class="container">
+      <div class="nav-bar">
+       
+        <nav class="navegation">
+          <a href="#">INICIO</a>
+          <a href="#">NOSOTROS</a>
+          <a href="#" class="opc">OFERTA EDUCATIVA</a>
+          <a href="/" class="py-2">
+            <img src="img/logo.png" class="logo" alt="Logotipo de la escuela">
+          </a>
+          <a href="#">INSCRIPCIONES</a>
+          <a href="#">INSTALACIONES</a>
+          <a href="#">CONTACTO</a>
+          </ul>
+        </nav>
+      </div>
     </div>
-    <div class="carousel-item">
-      <img src="img/img2.png" class="d-block w-100 img-slide" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="img/img3.png" class="d-block w-100 img-slide" alt="...">
-    </div>
-  </div>
-</div>
-<!-- 
-<div>
-    <img src="img/img1.svg" alt="">
-    <img src="img/img2.svg" alt="">
-    <img src="img/img3.svg" alt="">
-    <img src="img/img4.svg" alt="">
-    <img src="img/img5.svg" alt="">
-</div> -->
+  </header>
 
+  <section>
+    <h1>VILLA INFANTIL MARÍA ISABEL</h1>
+    <h2>Slogan</h2>
+  </section>
 
-<!--Archivos javascript para bootstrap --> 
-    <script src="js/jquery-3.4.1.min.js"></script>
-    <script src="js/popper.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+  <main>
+    <h2>Bienvenid@s</h2>
+    <hr>
+    <p>El jardín de niños Villa Infantil María Isabel le da las más cordial bienvenida a sus pequeños.
+      Ponemos a su disposición los servicios para el adecuado desarrollo de sus hijos en diversas
+      áreas como a convivencia, idiomas y valores para que sus niños tengan un desarrollo sano y
+      obtengan las habilidades que les permitirán continuar con su proceso de maduración.</p>
+    <img src="img/mascotakinder.png" alt="Logotipo de la mascota de la escuela">
+  </main>
+
+  <section>
+    <h3>Conoce nuestra oferta educativa así como los diferentes servicios con los que contamos</h3>
+    <a href="#" class="btn btn-primary">Ver más ></a>
+  </section>
+
+  <section>
+    <h2>Avisos</h2>
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="img/av1.jpg" class="d-block" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="img/av2.jpg" class="d-block" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="img/av3.jpg" class="d-block" alt="...">
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+  </section>
+
+  <section class="container mx-auto">
+    <h2 class="text-center">Últimos eventos</h2>
+    <div class="row">
+      <div class="col-12 col-md-4">
+        <article class="card">
+          <img src="img/img1.jpg" class="card-img-top" alt="Imagen ilustrativa del evento">
+          <div class="card-body">
+            <h5 class="card-title">Título del evento</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Leer más</a>
+          </div>
+        </article>
+      </div>
+      <div class="col-12 col-md-4">
+        <article class="card">
+          <img src="img/img1.jpg" class="card-img-top" alt="Imagen ilustrativa del evento">
+          <div class="card-body">
+            <h5 class="card-title">Título del evento</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Leer más</a>
+          </div>
+        </article>
+      </div>
+      <div class="col-12 col-md-4">
+        <article class="card">
+          <img src="img/img1.jpg" class="card-img-top" alt="Imagen ilustrativa del evento">
+          <div class="card-body">
+            <h5 class="card-title">Título del evento</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Leer más</a>
+          </div>
+        </article>
+      </div>
+    </div>
+  </section>
+
+  <section>
+    <h3>Frase</h3>
+    <p>Para ofrecerte más informes acerca de nuestros servicios, llena el siguiente formulario
+      y un asesor educativo se pondrá en contacto contigo a la brevedad.
+    </p>
+    <a href="#" class="btn btn-primary">Contáctanos ></a>
+  </section>
+
+  <section>
+    <h2>Testimonios</h2>
+    <div>
+      <h5>Nombre de la persona</h5>
+      <hr>
+      <blockquote>
+        Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type
+        and scrambled it to make a type specimen book.
+      </blockquote>
+      <div class="calificion">
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+      </div>
+    </div>
+    <div>
+      <h5>Nombre de la persona</h5>
+      <hr>
+      <blockquote>
+        Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type
+        and scrambled it to make a type specimen book.
+      </blockquote>
+      <div class="calificion">
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+      </div>
+    </div>
+    <div>
+      <h5>Nombre de la persona</h5>
+      <hr>
+      <blockquote>
+        Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type
+        and scrambled it to make a type specimen book.
+      </blockquote>
+      <div class="calificion">
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+      </div>
+    </div>
+  </section>
+
+  <footer>
+    <div>
+      <img src="img/mascotakinder.png" alt="Logotipo de la mascota del kinder">
+    </div>
+    <div>
+      <h5>Enlaces de interés</h5>
+      <hr>
+      <ul>
+        <li><a href="#">Nosotros</a></li>
+        <li><a href="#">Oferta Educativa</a></li>
+        <li><a href="#">Inscripciones</a></li>
+        <li><a href="#">Instalaciones y servicios</a></li>
+        <li><a href="#">Nivel Primaria </a></li>
+      </ul>
+    </div>
+    <div>
+      <h5>Contacto</h5>
+      <i class="fas fa-map-marker-alt"></i>
+      <p>Morelos 11, Santa Fe. Alvaro Obregon, CP 01210 Ciudad de México, CDMX</p>
+      <i class="fas fa-envelope"></i>
+      <p>colegiojrf@live.com.mx</p>
+      <i class="fas fa-phone"></i>
+      <p>55 70 59 39</p>
+      <i class="fab fa-facebook"></i>
+      <i class="fab fa-twitter"></i>
+    </div>
+    <div>
+      <p>&copy;2020 Todos los derechos reservados</p>
+      <nav>
+        <a href="#">Aviso de Privacidad</a>|
+        <a href="#">Otro documento</a>|
+        <a href="#">Otro documento</a>
+      </nav>
+    </div>
+  </footer>
+
+  <!--Archivos javascript para bootstrap -->
+  <script src="js/jquery-3.4.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
 </body>
+
 </html>

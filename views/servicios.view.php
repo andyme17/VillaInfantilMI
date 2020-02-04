bd3f4bb571cb78ed6ccbc76ff6fbaaceed7ff6f3
Andrea Morales Escobar <ame171337@gmail.com>
2020-02-03 22:20:36 -0600

    se actualiza pag servicios

--- views/servicios.view.php
+++ views/servicios.view.php
@@ -17,42 +17,122 @@
 
     <section class="img-back-blue h-md-back-3 py-4">
         <div class="content-back-blue container mx-auto">
-            <h2>Oferta Educativa</h2>
-            <hr>
-            <p class="text-back-blue">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum, et nam tenetur reiciendis minus nesciunt rem
-                amet impedit nulla qui, fugiat earum perferendis iusto vitae.(definir).
-            </p>
+            <div class="row">
+                <div class="col-12">
+                    <h2>Oferta Educativa</h2>
+                    <hr>
+                    <p class="text-back-blue">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum, et nam tenetur reiciendis minus nesciunt rem
+                        amet impedit nulla qui, fugiat earum perferendis iusto vitae.(definir).
+                    </p>
+                </div>
+            </div>
         </div>
     </section>
 
     <main class="py-4">
-        <h2 class="text-center">Modelo Educativo</h2>
-        <div class="container">          
-            
+        <div class="container text-center">
+            <div class="row">
+                <div class="col-12">
+                    <h2 class="text-center">Modelo Educativo</h2>
+                    <p class="pt-3 text-horario">En el horario de 09:00 a 14:00 hrs, ofrecemos: </p>
+                </div>
+            </div>
+            <div class="row pt-4 content-model">
+                <div class="col-12 col-md-4 pb-4">
+                    <img src="<?php echo PATH; ?>img/img1-service.png" alt="" class="img-fluid img-serv">
+                    <p>Programa establecido por la SEP</p>
+                </div>
+                <div class="col-12 col-md-4 pb-4">
+                    <img src="<?php echo PATH; ?>img/img2-service.jpg" alt="" class="img-fluid img-serv rounded-circle">
+                    <p>Inglés</p>
+                </div>
+                <div class="col-12 col-md-4 pb-4">
+                    <img src="<?php echo PATH; ?>img/img3-service.jpg" alt="" class="img-fluid img-serv rounded-circle">
+                    <p>Francés</p>
+                </div>
+                <div class="col-12 col-md-4 pb-4">
+                    <img src="<?php echo PATH; ?>img/img4-service.jpg" alt="" class="img-fluid img-serv rounded-circle">
+                    <p>Educación Física</p>
+                </div>
+                <div class="col-12 col-md-4 pb-4">
+                    <img src="<?php echo PATH; ?>img/img5-service.jpg" alt="" class="img-fluid img-serv rounded-circle">
+                    <p>Educación Musical</p>
+                </div>
+                <div class="col-12 col-md-4">
+                    <img src="<?php echo PATH; ?>img/img6-service.jpg" alt="" class="img-fluid img-serv rounded-circle">
+                    <p class="pt-1">Computación</p>
+                </div>
+            </div>
         </div>
     </main>
     
+    <div class="sec-after py-4">
+        <div class="container text-center">
+            <h3 class="text-center">Clases Afterschool</h3>
+            <p class="pt-3">
+                Pondremos a su disposición una oferta de clases especiales, fuera de
+                horario escolar, con el fin de complementar el desarrollo de sus hijos.
+            </p>
+            <div class="content-after container pt-3">
+                <div class="item-extra mb-3">
+                    <img src="<?php echo PATH; ?>img/img7-service.jpg" alt="Imagen ilustrativa de lima lama" class="img-fluid">
+                    <div class="content-extra">
+                        <h4>Lima Lama</h4>
+                    </div>
+                </div>
+                <div class="item-extra mb-3">
+                    <img src="<?php echo PATH; ?>img/img8-service.jpg" alt="Imagen ilustrativa de lima lama" class="img-fluid">
+                    <div class="content-extra">
+                        <h4>Zumba Kids</h4>
+                    </div>
+                </div>
+                <div class="item-extra mb-3">
+                    <img src="<?php echo PATH; ?>img/img9-service.jpg" alt="Imagen ilustrativa de lima lama" class="img-fluid">
+                    <div class="content-extra">
+                        <h4>Taekwondo</h4>
+                    </div>
+                </div>
+                <div class="item-extra mb-3">
+                    <img src="<?php echo PATH; ?>img/img10-service.jpg" alt="Imagen ilustrativa de lima lama" class="img-fluid">
+                    <div class="content-extra">
+                        <h4>Regularización en Inglés</h4>
+                    </div>
+                </div>
+                <div class="item-extra mb-3">
+                    <img src="<?php echo PATH; ?>img/img11-service.jpg" alt="Imagen ilustrativa de lima lama" class="img-fluid">
+                    <div class="content-extra">
+                        <h4>Regularización en Francés</h4>
+                    </div>
+                </div>
+            </div>           
+        </div>
+    </div>
+
     <section class="img-inscrip py-4">
         <div class="container content-inscrip mx-auto">
-            <h4 class="mb-3">¿Te interesa formar parte de la comunidad de Villa Infantil María Isabel?(definir)</h4>
-            <h5>Checa nuestro proceso de inscripción(definir).</h5>
+            <h4 class="mb-3">¿Te interesa formar parte de la comunidad de Villa Infantil María Isabel?</h4>
+            <h5>Inscríbete y se parte de nuestra comunidad</h5>
             <a href="<?php echo PATH; ?>inscripciones.php" class="btn btn-outline-light mt-4">Click aquí ></a>
         </div>
     </section>
 
-   <section class="img-back py-4">
+    <section class="img-back py-4">
         <div class="content-service container mx-auto">
             <div class="row">
-                <div class="col-12 col-md-6 col-xl-7 order-2 ">
-                    <img class="img-fluid pt-3 pt-md-0" src="<?php echo PATH; ?>img/img-servicio-comida.jpg" alt="Foto del servicio de comida del preescolar">
+                <div class="col-12 col-md-8 offset-md-2 col-lg-7 offset-lg-0 col-xl-7 order-2 ">
+                    <img class="img-fluid pt-3 pt-lg-0" src="<?php echo PATH; ?>img/img-servicio-comida.jpg" alt="Foto del servicio de comida del preescolar">
                 </div>
-                <div class="col-12 col-md-6 col-xl-5 order-md-2 pt-xl-5">
-                    <h3>Servicio de Comida</h3>
+                <div class="col-12 col-lg-5 order-lg-2 py-xl-4">
+                    <h3>Comedor</h3>
                     <p>
-                        En Villa Infantil María Isabel ponemos a su disposición el servicio de desayuno en horario regular 
-                        y comida en el periodo afterclass,  a fin de poder brindarles una alternativa cómoda y saludable para este
-                        propósito, con menús variados y balanceados, asegurando una buena alimentación para sus pequeños.
+                        A partir del mes de febrero, pondremos a su disposición el servicio de comedor dentro de las instalaciones
+                        de Villa Infantil, a fin de poder brindarles una alternativa cómoda y saludable para este propósito.
+                    </p>
+                    <h5 class="mt-3 mt-xl-4">Menu Variados</h5>
+                    <p>
+                        Con asesoría nutricional, tendremos menús variados y balanceados, para asegurar un aporte calórico adecuado,
+                        además de un excelente sabor para los niños.
                     </p>
                 </div>
             </div>
@@ -60,16 +140,16 @@
     </section>
 
     <section class="sec-contacto py-4">
-    <div class="container">
-      <div class="header-contacto mb-3">
-        <p class="mb-3 mb-md-0">¿Tienes dudas acerca de nuestro servicio?(definir)</p>
-      </div>
-      <p>
-        Comunicate con nosotros llenando el siguiente formulario. (definir)
-      </p>
-      <a href="<?php echo PATH; ?>contacto.php" class="btn btn-outline-light mt-4">Contáctanos ></a>
-    </div>
-  </section>
+        <div class="container">
+            <div class="header-contacto mb-3">
+                <p class="mb-3 mb-md-0">¿Tienes dudas acerca de nuestro servicio?</p>
+            </div>
+            <p>
+                Comunicate con nosotros llenando el siguiente formulario.
+            </p>
+            <a href="<?php echo PATH; ?>contacto.php" class="btn btn-outline-light mt-4">Contáctanos ></a>
+        </div>
+    </section>
     <?php require 'footer.view.php'; ?>
 
     <!--Archivos javascript para bootstrap -->
@@ -79,4 +159,5 @@
     <script src="js/headroom.min.js"></script>
     <script src="js/main.js"></script>
 </body>
-</html>    
+
+</html>

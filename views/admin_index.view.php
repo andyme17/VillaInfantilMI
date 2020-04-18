09628e5e0b143ea9835eecf194ace0807c8e4ddf
unknown <ame171337@gmail.com>
2020-04-18 17:10:06 -0500

    primera version de panel de control

--- views/admin_index.view.php
+++ views/admin_index.view.php
@@ -6,14 +6,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="google-site-verification" content="x7i-1eQfu-xEuD6jxgrEJD3LJAi5do0R7iRzlfy9Y_4" />
-    <title>Villa Infantil María Isabel</title>
-    <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/all.min.css">
-    <link rel="stylesheet" href="css/style.css">
+    <title>Gestor de contenido VillaMI</title>
+    <link rel="shortcut icon" type="image/x-icon" href="../img/favicon.ico">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/all.min.css">
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 
 <body id="login">
-    <a href="<?php echo PATH; ?>cerrar.php">Cerrar Sesión</a>
+    <div class="wrapper">
+        <?php require 'side_bar.view.php'; ?>
+        <div id="content" class="img-back">
+            <div class="content-init">
+                <h1 class="my-5 text-center">Bienvenido</h1>
+                <div class="img-init">
+                    <img src="<?php echo PATH; ?>img/logo-kinder.png" class="img-fluid">
+                    <img src="<?php echo PATH; ?>img/logo-mascota-kinder.png" class="img-fluid">
+                </div>
+            </div>
+        </div>
+
+       
+    </div>
+
+    <!-- JavaScript files for Bootstrap 4 -->
+    <script src="../js/jquery-3.4.1.min.js"></script>
+    <script src="../js/popper.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/headroom.min.js"></script>
 </body>
-</html>    
+
+</html>
---
+++ views/side_bar.view.php
@@ -0,0 +1,74 @@
+    <nav id="sidebar">
+        <div class="sidebar-header">
+            <i class="fas fa-user-circle"></i>
+            <p>Administrador</p>
+        </div>
+
+        <ul>
+            <p>Páginas</p>
+            <li class="active">
+                <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><i class="fas fa-home"></i> Inicio</a>
+                <ul class="collapse" id="homeSubmenu">
+                    <li>
+                        <a href="#">Mensaje de bienvenida</a>
+                    </li>
+                    <li>
+                        <a href="#">Avisos</a>
+                    </li>
+                    <li>
+                        <a href="#">Testimonios</a>
+                    </li>
+                    <li>
+                        <a href="#">Eventos</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><i class="fas fa-users"></i> Nosotros</a>
+                <ul class="collapse" id="pageSubmenu">
+                    <li>
+                        <a href="#">Mensaje de inicio</a>
+                    </li>
+                    <li>
+                        <a href="#">Visión</a>
+                    </li>
+                    <li>
+                        <a href="#">Nuestro equipo</a>
+                    </li>
+                    <li>
+                        <a href="#">Personal Administrativo</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="#pageServicios" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><i class="fas fa-tasks"></i> Servicios</a>
+                <ul class="collapse" id="pageServicios">
+                    <li>
+                        <a href="#">Nuevo servicio</a>
+                    </li>
+                    <li>
+                        <a href="#">Eliminar servicio</a>
+                    </li>
+                    <li>
+                        <a href="#">Modificar servicio</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="#pageInsta" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><i class="fas fa-school"></i> Instalaciones</a>
+                <ul class="collapse" id="pageInsta">
+                    <li>
+                        <a href="#">Nueva imagen</a>
+                    </li>
+                    <li>
+                        <a href="#">Eliminar imagen</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="cerrar">
+                <a href="<?php echo PATH; ?>cerrar.php">Cerrar sesión <i class="fas fa-sign-out-alt"></i></a>
+            </li>
+        </ul>
+    </nav>
+
+  
